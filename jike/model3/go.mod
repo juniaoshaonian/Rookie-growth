1320b777f8444c5df528ad6d3328c7579ce44e41
zwl <1633720889@qq.com>
2022-05-29 23:46:07 +0800

    model3

---
+++ jike/model3/go.mod
@@ -0,0 +1,5 @@
+module model3
+
+go 1.16
+
+require golang.org/x/sync v0.0.0-20220513210516-0976fa681c29
--- jike/model3/main.go
+++ jike/model3/main.go
@@ -1,1 +1,59 @@
-package model3
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func  main(){
+	g, ctx := errgroup.WithContext(context.Background())
+	h:= http.NewServeMux()
+	h.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
+		w.Write([]byte("hi"))
+	})
+	shutdown := make(chan struct{})
+	h.HandleFunc("/stop",func(w http.ResponseWriter,r *http.Request){
+		shutdown <- struct{}{}
+	})
+	server := http.Server{
+		Handler: h,
+		Addr: ":8080",
+	}
+	g.Go(func() error {
+		return server.ListenAndServe()
+	})
+	g.Go(func() error {
+		select {
+		case <-ctx.Done():
+			fmt.Println("errgroup exit...")
+		case <-shutdown:
+			fmt.Println("server will out...")
+		}
+
+		Ctx, cancel := context.WithCancel(context.Background())
+
+		defer cancel()
+
+		fmt.Println("shutting down server...")
+		return server.Shutdown(Ctx)
+	})
+	g.Go(func() error {
+		quit := make(chan os.Signal, 0)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case sig := <-quit:
+			return errors.New(fmt.Sprintf("os exited%v",sig))
+		}
+	})
+
+	fmt.Printf("errgroup exiting: %+v\n", g.Wait())
+}
